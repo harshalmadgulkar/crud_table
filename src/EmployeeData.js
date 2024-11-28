e520a2010748d82ee5e7dc3f7f8f59e469a239dc
unknown <harshal.madgulkar725@gmail.com>
2024-11-28 08:38:25 +0530

    first commit

---
+++ src/EmployeeData.js
@@ -0,0 +1,5 @@
+const data = [
+  { id: 1, firstname: "Mark", lastname: "David", age: 25 },
+  { id: 2, firstname: "Virat", lastname: "Kohali", age: 34 },
+  { id: 3, firstname: "Mark", lastname: "David", age: 35 },
+];
